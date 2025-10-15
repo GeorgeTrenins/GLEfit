fix sign error (undetected by prior testing due to error cancellation)

src/glefit/embedding/prony.py
@@ -69,14 +69,14 @@ class PronyEmbedder(BaseEmbedder):
         -------
         Drift matrix for the Prony embedding,
            [ 0   θ ]
-           [ θ   γ ]
+           [-θ   γ ]
         """
         theta, gamma = np.asarray(params)
         A = np.zeros((2,2))
-        A[0,0] = 0.0
-        A[0,1] = theta
-        A[1,0] = theta
-        A[1,1] = gamma
+        A[0,0] =  0.0
+        A[0,1] =  theta
+        A[1,0] = -theta
+        A[1,1] =  gamma
         return A
     
     def drift_matrix_param_grad(
@@ -89,10 +89,10 @@ class PronyEmbedder(BaseEmbedder):
         -------
         Derivative of the drift matrix
            [ 0   θ ]
-           [ θ   γ ]
+           [-θ   γ ]
         with respect to θ (grad[0]):
            [ 0   1 ]
-           [ 1   0 ]
+           [-1   0 ]
         and γ (grad[1]):
            [ 0   0 ]
            [ 0   1 ]
@@ -100,7 +100,7 @@ class PronyEmbedder(BaseEmbedder):
         grad_A = np.zeros((2,2,2))
         # Derivative with respect to θ
         grad_A[0,0,1] = 1.0
-        grad_A[0,1,0] = 1.0
+        grad_A[0,1,0] =-1.0
         # Derivative with respect to γ
         grad_A[1,1,1] = 1.0
         return grad_A
@@ -120,9 +120,9 @@ class PronyEmbedder(BaseEmbedder):
         theta, gamma = self.params
         exp_gamma_t = np.exp(-gamma*time)
         ans = np.empty((2, len(time)))
-        # First derivative term: d/dθ [θ^2 exp(-γt)]
+        # d/dθ
         ans[0] = 2 * theta * exp_gamma_t
-        # Second derivative term: d/dγ [θ^2 exp(-γt)]
+        # d/dγ
         ans[1] = -(theta**2) * time * exp_gamma_t
         return ans
     
@@ -134,11 +134,11 @@ class PronyEmbedder(BaseEmbedder):
         theta, gamma = self.params
         exp_gamma_t = np.exp(-gamma*time)
         ans = np.empty((2, 2, len(time)))
-        # d²/dθ² [θ^2 exp(-γt)]
+        # d²/dθ² 
         ans[0,0] = 2*exp_gamma_t
-        # d²/dθdγ [θ^2 exp(-γt)]
+        # d²/dθdγ
         ans[0,1] = ans[1,0] = -2 * theta * time * exp_gamma_t
-        # d²/dγ² [θ^2 exp(-γt)]
+        # d²/dγ² 
         ans[1,1] = theta**2 * time**2 * exp_gamma_t
         return ans
     
@@ -156,9 +156,8 @@ class PronyEmbedder(BaseEmbedder):
         """
         theta, gamma = self.params
         ans = np.empty((2, len(frequency)))
-        # First derivative term: d/dθ [γ θ^2 / (γ^2 + ω^2)] = 2 θ γ / (γ^2 + ω^2)
+        # d/dθ [γ θ^2 / (γ^2 + ω^2)] = 2 θ γ / (γ^2 + ω^2)
         ans[0] = 2 * theta * gamma / (gamma**2 + frequency**2)
-        # Second derivative term: 
         # d/dγ [γ * θ^2 / (γ^2 + ω^2)] = θ^2 * (ω^2 - γ^2) / (γ^2 + ω^2)^2
         ans[1] = theta**2 * (frequency**2 - gamma**2) / (gamma**2 + frequency**2)**2
         return ans
@@ -173,10 +172,10 @@ class PronyEmbedder(BaseEmbedder):
         w2 = frequency**2
         gw2 = g2 + w2
         ans = np.empty((2, 2, len(frequency)))
-        # d²/dθ² [θ^2 exp(-γt)]
+        # d²/dθ² 
         ans[0,0] = 2*gamma / gw2
-        # d²/dθdγ [θ^2 exp(-γt)]
+        # d²/dθdγ 
         ans[0,1] = ans[1,0] = 2*theta * (w2 - g2) / gw2**2
-        # d²/dγ² [θ^2 exp(-γt)]
+        # d²/dγ²
         ans[1,1] = 2*gamma*theta**2 * (g2 - 3*w2) / gw2**3
         return ans

src/glefit/embedding/pronycos.py
@@ -72,8 +72,8 @@ class PronyCosineEmbedder(BaseEmbedder):
         -------
         Drift matrix for the oscillatory Prony embedding,
            [ 0   θ   0]
-           [ θ   γ  -ω]
-           [ 0   ω   γ]
+           [-θ   γ   ω]
+           [ 0  -ω   γ]
         """
         theta, gamma, omega = np.asarray(params)
         A = np.zeros((3,3))
@@ -81,8 +81,8 @@ class PronyCosineEmbedder(BaseEmbedder):
         A[0,1] = theta
         A[1,0] = -theta
         A[1,1] = A[2,2] = gamma
-        A[1,2] = -omega
-        A[2,1] = omega
+        A[1,2] = omega
+        A[2,1] = -omega
         return A
     
     def drift_matrix_param_grad(
@@ -95,19 +95,19 @@ class PronyCosineEmbedder(BaseEmbedder):
         -------
         Derivative of the drift matrix
            [ 0   θ   0]
-           [ θ   γ  -ω]
-           [ 0   ω   γ]
+           [-θ   γ   ω]
+           [ 0  -ω   γ]
         with respect to θ, γ, and ω (grad[0], grad[1], grad[2])
         """
         grad_A = np.zeros((3,3,3))
-        # Derivative with respect to θ
+        # d/dθ 
         grad_A[0,0,1] = 1.0
-        grad_A[0,1,0] = 1.0
-        # Derivative with respect to γ
+        grad_A[0,1,0] = -1.0
+        # d/dγ 
         grad_A[1,1,1] = grad_A[1,2,2] = 1.0
-        # Derivative with respect to omega
-        grad_A[2,1,2] = -1.0
-        grad_A[2,2,1] = 1.0
+        # d/dω 
+        grad_A[2,1,2] = 1.0
+        grad_A[2,2,1] = -1.0
         return grad_A
     
     def kernel_func(self, time: ScalarArr) -> npt.NDArray[np.floating]:
@@ -128,11 +128,11 @@ class PronyCosineEmbedder(BaseEmbedder):
         exp_gamma_t = np.exp(-gamma*time)
         cos_omega_t = np.cos(omega*time)
         ans = np.empty((3, len(time)))
-        # d/dθ [θ^2 exp(-γt) cos(ωt)]
+        # d/dθ 
         ans[0] = 2 * theta * exp_gamma_t * cos_omega_t
-        # d/dγ [θ^2 exp(-γt) cos(ωt)]
+        # d/dγ 
         ans[1] = -(theta**2) * time * exp_gamma_t * cos_omega_t
-        # d/dω [θ^2 exp(-γt) cos(ωt)]
+        # d/dω 
         ans[2] = -(theta**2) * time * exp_gamma_t * np.sin(omega*time)
         return ans
     
@@ -147,17 +147,17 @@ class PronyCosineEmbedder(BaseEmbedder):
         cos_omega_t = np.cos(omega*time)
         sin_omega_t = np.sin(omega*time)
         ans = np.empty((3, 3, len(time)))
-        # d²/dθ² [θ^2 exp(-γt) cos(ωt)]
+        # d²/dθ² 
         ans[0,0] = 2 * exp_gamma_t * cos_omega_t
-        # d²/dθdγ [θ^2 exp(-γt) cos(ωt)]
+        # d²/dθdγ 
         ans[0,1] = ans[1,0] = -2 * theta * time * exp_gamma_t * cos_omega_t
-        # d²/dθdω [θ^2 exp(-γt) cos(ωt)]
+        # d²/dθdω 
         ans[0,2] = ans[2,0] = -2 * theta * time * exp_gamma_t * sin_omega_t
-        # d²/dγ² [θ^2 exp(-γt) cos(ωt)]
+        # d²/dγ² 
         ans[1,1] = theta**2 * time**2 * exp_gamma_t * cos_omega_t
-        # d²/dγdω [θ^2 exp(-γt) cos(ωt)]
+        # d²/dγdω 
         ans[1,2] = ans[2,1] = theta**2 * time**2 * exp_gamma_t * sin_omega_t
-        # d²/dω² [θ^2 exp(-γt) cos(ωt)]
+        # d²/dω² 
         ans[2,2] = -(theta**2) * time**2 * exp_gamma_t * cos_omega_t
         return ans
     

src/glefit/merit/properties.py
@@ -67,6 +67,6 @@ class MemoryKernel(BaseArrayProperty):
         A = Ap[1:,1:]
         expA, grad_expA = self._grad_thetaT_expA_theta(A, theta, time)
         ans[:,0,1:] = np.einsum('j,tjk->tk', theta, expA)
-        ans[:,1:,0] = ans[:,0,1:]
+        ans[:,1:,0] = -ans[:,0,1:]
         ans[:,1:,1:] = grad_expA
         return ans